1221f3172bbdea1e6cc9c69e595adeaa145b5e91
ah-ryeong <aryeong22@hanmail.net>
2021-11-01 00:31:32 +0900

    컨테이너 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
+import GithubProfileLoader from './containers/GithubProfileLoader';
 import TodoApp from './containers/TodoApp';
 
 const App: React.FC = () => {
@@ -7,6 +8,7 @@ const App: React.FC = () => {
     <>
      <CounterContainer />
      <TodoApp />
+     <GithubProfileLoader />
     </>
   );
 }
