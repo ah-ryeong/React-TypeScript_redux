2a308a00f02d998fc096c6e8d3e79b448e2b9348
ah-ryeong <aryeong22@hanmail.net>
2021-10-31 22:14:04 +0900

    typesafe-actions

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,13 +1,7 @@
-const INCREASE = 'counter/INCREASE' as const;
-const DECREASE = 'counter/DECREASE' as const;
-const INCREASE_BY = 'counter/INCREASE_BY' as const;
-
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-export const increaseBy = (diff: number) => ({
-    type: INCREASE_BY,
-    payload: diff,
-});
+import { createAction, ActionType, createReducer } from 'typesafe-actions';
+export const increase = createAction('counter/INCREASE')();
+export const decrease = createAction('counter/DECREASE')();
+export const increaseBy = createAction('counter/INCREASE_BY')<number>();
 
 type CounterState = {
     count: number;
@@ -17,22 +11,13 @@ const initialState = {
     count: 0
 };
 
-type CounterAction = 
-    | ReturnType<typeof increase>
-    | ReturnType<typeof decrease>
-    | ReturnType<typeof increaseBy>
+const actions = { increase, decrease, increaseBy };
 
-function counter(state: CounterState = initialState, action: CounterAction): CounterState {
-    switch (action.type) {
-        case INCREASE:
-            return { count: state.count +1 };
-        case DECREASE:
-            return { count: state.count - 1 };
-        case INCREASE_BY:
-            return { count: state.count + action.payload };
-        default:
-            return state;
-    }
-}
+type CounterAction = ActionType<typeof actions>;
+
+const counter = createReducer<CounterState, CounterAction>(initialState)
+    .handleAction(increase, state => ({ count: state.count + 1}))
+    .handleAction(decrease, state => ({ count: state.count -1 }))
+    .handleAction(increaseBy, (state, action ) => ({ count: state.count + action.payload }));
 
 export default counter;
